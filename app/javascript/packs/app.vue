e21f2154a4af3706c79d1946fab551624cfe0b92
may88seiji <may88seiji@gmail.com>
2018-07-03 01:24:40 +0900

    fix bug

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -8,7 +8,7 @@
 export default {
   data: function () {
     return {
-      message: "Hello"
+      message: "Hello Vue!"
     }
   }
 }
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const vue =  require('./loaders/vue')
 
-environment.loaders.append('vue', vue)
+environment.loaders.get('vue').options.extractCSS = false
+
 module.exports = environment
